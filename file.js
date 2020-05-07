4b3133214bb3840248457845a1828bc628b068b1
lublubczyk <57983570+lublubczyk@users.noreply.github.com>
2020-05-07 15:31:12 +0300

    Add files via upload

---
+++ file.js
@@ -0,0 +1,15 @@
+let city = "Kuiv";
+    city = "Lviv";
+let address = city;
+    alert(address);   
+
+let n = prompt("Введіть число n і отримайте його в кубі в консольці...");
+let result = n*n*n;
+console.log(result);
+
+
+let x = prompt("Кількість олівців - ");
+let y = prompt("Кількість ручок - ");
+let pricePen = 5.25;
+let pricePencil = 3.5;
+document.write(`<h2>З Вас ${x*pricePen + y*pricePencil}</h2>`)
